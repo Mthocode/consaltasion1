00c2202d35ae521a1fe637a85839640da4885c80
Mthokozis <gameheart312@gmail.com>
2026-04-06 12:15:43 +0100

    mobile css

---
+++ js/mobile_nav.js
@@ -0,0 +1,21 @@
+const barsIcon = document.querySelector('.fa-bars');
+const xmarkIcon = document.querySelector('.fa-xmark');
+const navMenu = document.querySelector('.top-bar > nav');
+
+barsIcon.addEventListener('click', () => {
+    
+        console.log('Bars icon clicked');
+        navMenu.classList.add('active');
+        navMenu.style.display = 'block';
+        barsIcon.style.display = 'none';
+        xmarkIcon.style.display = 'block';
+    });
+
+xmarkIcon.addEventListener('click', () => {
+    
+        console.log('Xmark icon clicked');
+        navMenu.classList.remove('active');
+        navMenu.style.display = 'none';
+        xmarkIcon.style.display = 'none';
+        barsIcon.style.display = 'block';
+    });
